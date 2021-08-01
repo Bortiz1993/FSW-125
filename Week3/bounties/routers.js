e76933eef442e3ba7c800a90d2911d8a67acfcd1
Bortiz1993 <brijido.zuniga@bryanuniversity.edu>
2021-08-01 05:32:48 -0500

    update2

--- Week3/bounties/routers.js
+++ Week3/bounties/routers.js
@@ -5,6 +5,7 @@ const  {v4: uuidv4 } = require('uuid');
 const bounties = [
     {FirstName: 'Ray', 
      LastName: 'SkyWalker',
+     LightSaber: 'Green',
      Living: true,
      BAmount: 100,
      Type: 'Jedi',
@@ -13,26 +14,55 @@ const bounties = [
 
 ];
 
-
-//Post to add something new
-bountiesRouter.post(`/`, (req, res) => {
-    const newBountie = req.body;
-    newBountie._id = uuidv4();
-    bounties.push(newBountie);
-
-    console.log(bounties)
-    res.send(`Successfully added ${newBountie.FirstName} to the database`);
-})
-
-
 //Get all route
 bountiesRouter.get(`/`, (req, res) => {
     res.send(bounties)
 });
 
-bountiesRouter.get('/:bountiesId', (req, res) =>{
+ bountiesRouter.get('/:bountiesId', (req, res) =>{
+     const bountiesId = req.params.bountiesId;
+    const singleBounty = bounties.find(bounty => bounty._id === bountiesId)
     console.log(req.params)
+
+     res.send(singleBounty);
+ })
+
+ .get('/search/LightSaber',(req, res) => {
+     const bountiesLightSaber = req.query.LightSaber;
+     const filteredLightSabers = bounties.filter(bounty => bounty.LightSaber === bountiesLightSaber)
+    
+     res.send(filteredLightSabers)
+    console.log(re.query);
+ })
+
+
+
+//post a route
+
+bountiesRouter.post(`/`, (req, res) => {
+    const newBountie = req.body;
+    newBountie._id = uuidv4();
+    bounties.push(newBountie);
+
+    res.send(`Successfully added ${newBountie.FirstName} to the database`);
 })
+
+.delete('/:bountiesId', (req, res) =>{
+    const bountiesId = req.params.bountiesId;
+    const bountiesIndex = bounties.findIndex(bounty => bounty._id === bountiesId);
+    bounties.splice(bountiesIndex, 1);
+
+    res.send('Successfully deleted!')
+})
+
+.put('/:bountiesId', (req,res) => {
+    const bountiesId = req.params.bountiesId;
+    const bountiesIndex = bounties.findIndex(bounty => bounty._id === bountiesId);
+     Object.assign(bounties[bountiesIndex], req.body)
+
+    res.send('Successfully updated');
+})
+
 
 module.exports = bountiesRouter;
 
